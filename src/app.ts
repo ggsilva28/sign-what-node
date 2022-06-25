bc3cfdc018db5d18705361464c9ebf6159264d4a
Gabriel Gonçalves <gagsilva28@gmail.com>
2022-06-25 15:49:21 -0300

    Here goes something

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import http from "http"
+import cors from "cors"
+
+import { router } from "./routes"
+
+const app = express()
+app.use(cors())
+
+const serverHttp = http.createServer(app)
+
+app.use(express.json())
+
+app.use(router)
+
+export { serverHttp }
